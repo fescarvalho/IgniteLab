37bce610fa49dcee459df4d6bb2378ad22a40a31
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2022-06-23 21:36:06 -0300

    CSS lessons actives(classnames)

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,5 +1,6 @@
 import { gql, useMutation } from "@apollo/client";
 import { useState, FormEvent } from "react"
+import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo"
 
   const CREATE_SUBSCRIBER_MUTATION = gql`
@@ -11,22 +12,23 @@ import { Logo } from "../components/Logo"
   `
 
 const Subscribe = () => {
+  const navigate = useNavigate()
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
-  const [createSubscriber] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+  const [createSubscriber, {loading}] = useMutation(CREATE_SUBSCRIBER_MUTATION)
 
 
-  function handleSubscribe(event:FormEvent) {
+  async function handleSubscribe(event:FormEvent) {
     event.preventDefault();
    
-    createSubscriber({
+   await  createSubscriber({
       variables: {
         name,
         email
       }
     }) 
-
+    navigate('/event')
   }
 
   return (
@@ -57,8 +59,9 @@ const Subscribe = () => {
            />
 
            <button 
+           disabled={loading}
            type="submit"
-           className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition:colors">
+           className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition:colors disabled:opacity-50">
             Garantir minha vaga
            </button>
 
